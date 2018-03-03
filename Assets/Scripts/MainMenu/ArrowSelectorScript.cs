0a99ba5bc54a968b2f9e3f9d5ee1ce2f33a45d6d
Thomas Faget <thomas.faget@hotmail.fr>
2018-03-03 01:37:56 -0500

    Add Lobby menu 1V1 (Prototype)

---
+++ Assets/Scripts/MainMenu/ArrowSelectorScript.cs
@@ -0,0 +1,51 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowSelectorScript : MonoBehaviour {
+
+    [SerializeField]
+    private Image image;
+
+    public CarSelectorScript carSelectorScript;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnEnter()
+    {
+        image.color = new Color32(220,220,220,255);
+    }
+
+    public void OnLeave()
+    {
+        image.color = Color.white;
+    }
+
+    public void OnSelect()
+    {
+        image.color = new Color32(200, 200, 200, 255);
+    }
+
+    public void OnClick()
+    {
+        switch (name)
+        {
+            case "ArrowLeft":
+                carSelectorScript.SetPreviousCar();
+                break;
+            case "ArrowRight":
+                carSelectorScript.SetNextCar();
+                break;
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/MainMenu/CarSelectorScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSelectorScript : MonoBehaviour
+{
+
+    public GameObject[] cars;
+    public GameObject curCar;
+    private int curCarIndex;
+    public Transform spawnPoint;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    curCar = Instantiate(cars[0], spawnPoint.position, spawnPoint.rotation);
+	    curCar.transform.localScale = Vector3.one*30;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetNextCar()
+    {
+        curCarIndex++;
+        if (curCarIndex == cars.Length)
+        {
+            curCarIndex = 0;
+        }
+        Destroy(curCar);
+        curCar = Instantiate(cars[curCarIndex], spawnPoint.position, spawnPoint.rotation);
+    }
+
+    public void SetPreviousCar()
+    {
+        curCarIndex--;
+        if (curCarIndex == -1)
+        {
+            curCarIndex = cars.Length-1;
+        }
+        Debug.Log("cur car : " + curCarIndex);
+        Destroy(curCar);
+        curCar = Instantiate(cars[curCarIndex], spawnPoint.position, spawnPoint.rotation);
+    }
+
+    public GameObject GetCurrentCar()
+    {
+        Destroy(curCar);
+        GameObject car = Instantiate(cars[0], spawnPoint.position, spawnPoint.rotation);
+        curCar = car;
+        return car;
+    }
+
+
+}
