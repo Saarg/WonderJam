195ba13dfc6050a547591b5b8115e0d1aec91fad
Thomas Faget <thomas.faget@hotmail.fr>
2018-03-03 16:58:10 -0500

    Add script to detect choosen controller

---
+++ Assets/ChooseControllerScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseControllerScript : MonoBehaviour
+{
+
+    public string name;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetController(int value)
+    {
+        PlayerPrefs.SetInt(name, value);
+    }
+}
