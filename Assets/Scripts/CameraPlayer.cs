b033cbe804e3b2c959aee5a8f9dfe12cd0552264
y3lousso <y3lousso@gmail.com>
2018-03-02 22:11:12 -0500

    Add CameraPlayer (speed feedback)

---
+++ Assets/Scripts/CameraPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayer : MonoBehaviour {
+
+    private WheelVehicle wheelVehicle;
+    private Vector3 baseLocalPosition;
+
+    [Range(0, 10)]
+    public float MaxDistance = 1;
+    public float CurrentDistance;
+
+    [Range(0, 100)]
+    public float maxSpeed;
+
+	// Use this for initialization
+	void Start () {
+        wheelVehicle = GetComponentInParent<WheelVehicle>();
+        baseLocalPosition = transform.localPosition;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        CurrentDistance = MaxDistance * (wheelVehicle.speed / maxSpeed);
+
+        transform.localPosition = baseLocalPosition - new Vector3(0, 0, CurrentDistance);
+    }
+}
